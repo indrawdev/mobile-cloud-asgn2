c626716bcb1727046ea40aff525c903de764d614
Indra Pramana <indra@ide.web.id>
2021-03-21 15:00:34 +0700

    add implement VideoSvcApi

--- src/main/java/org/magnum/mobilecloud/video/AnEmptyController.java
+++ src/main/java/org/magnum/mobilecloud/video/AnEmptyController.java
@@ -18,7 +18,14 @@
 
 package org.magnum.mobilecloud.video;
 
+import java.util.Collection;
+
+import org.magnum.mobilecloud.video.client.VideoSvcApi;
+import org.magnum.mobilecloud.video.repository.Video;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,9 +50,51 @@ public class AnEmptyController {
 	 * 
 	 */
 	
-	@RequestMapping(value="/go",method=RequestMethod.GET)
+	@RequestMapping(value="/go", method=RequestMethod.GET)
 	public @ResponseBody String goodLuck(){
 		return "Good Luck!";
+	}
+	
+	@GetMapping(value="/video")
+	public @ResponseBody Collection<Video> getVideoList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@GetMapping(value="/video/{id}")
+	public @ResponseBody Video getVideoById(@PathVariable("id") long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@PostMapping(value="/video")
+	public @ResponseBody Video addVideo(Video v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@PostMapping(value="/video/{id}/like")
+	public @ResponseBody Void likeVideo(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@PostMapping(value="/video/{id}/unlike")
+	public Void unlikeVideo(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@GetMapping(value="/video/search/findByName?title={title}")
+	public @ResponseBody Collection<Video> findByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@GetMapping(value="/video/search/findByDurationLessThan?duration={duration}")
+	public @ResponseBody Collection<Video> findByDurationLessThan(long duration) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- src/main/java/org/magnum/mobilecloud/video/Application.java
+++ src/main/java/org/magnum/mobilecloud/video/Application.java
@@ -10,7 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 public class Application {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
